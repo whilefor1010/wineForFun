860c096ac9656d3b0339a239b35f1c423aae3202
whilefor1010 <whilefor1010@gmail.com>
2023-03-15 14:40:00 +0200

    first commit

---
+++ src/main/java/com/whilefor1010/wineForFun/WineForFunApplication.java
@@ -0,0 +1,14 @@
+package com.whilefor1010.wineForFun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineForFunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WineForFunApplication.class, args);
+		System.out.println("Ok, go)");
+	}
+
+}
---
+++ src/test/java/com/whilefor1010/wineForFun/WineForFunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whilefor1010.wineForFun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineForFunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
