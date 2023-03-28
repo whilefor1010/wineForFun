7601d27cd0ed96a84bc85447746d1c99f630cac0
whilefor1010 <whilefor1010@gmail.com>
2023-03-29 01:30:33 +0300

    add login

--- src/main/java/com/whilefor1010/wineForFun/conf/WebSecurityConfig.java
+++ src/main/java/com/whilefor1010/wineForFun/conf/WebSecurityConfig.java
@@ -1,2 +1,53 @@
-package com.whilefor1010.wineForFun.conf;public class WebSecurityConfig {
+package com.whilefor1010.wineForFun.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home", "/catalog").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("p1")
+                        .roles("USER")
+                        .build();
+
+        UserDetails admin =
+                User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("p1")
+                        .roles("ADMIN")
+                        .build();
+
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
